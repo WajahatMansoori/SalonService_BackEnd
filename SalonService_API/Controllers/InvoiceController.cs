1a0d2d41ad0d2ec38435b3a4302290d38a62b41e
WajahatMansoori <Wajahatmansoori111@gmail.com>
2022-01-11 05:36:21 +0500

    Invoice and Product APIs

---
+++ SalonService_API/Controllers/InvoiceController.cs
@@ -0,0 +1,39 @@
+﻿using SalonService_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SalonService_API.Controllers
+{
+    public class InvoiceController : ApiController
+    {
+        SalonDBEntities db = new SalonDBEntities();
+
+        [HttpGet]
+        [Route("api/GetallInvoices")]
+        public IEnumerable<Admin_Select_Invoices_Result> GetAllInvoices()
+        {
+            return db.Admin_Select_Invoices();
+        }
+
+        [HttpPost]
+        [Route("api/InsertInvoice")]
+        public void AddInvoice(Invoice iv)
+        {
+            try
+            {
+                if (iv.SalonProductsId > 0 && iv.InvoiceAmount > 0 && iv.InvoiceDate != null && iv.ProductQuantity > 0)
+                {
+                    db.Admin_Insert_Invoices(iv.SalonProductsId, iv.InvoiceAmount, iv.InvoiceDate, iv.ProductQuantity);
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ SalonService_API/Controllers/SalonProductController.cs
@@ -0,0 +1,73 @@
+﻿using SalonService_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SalonService_API.Controllers
+{
+    public class SalonProductController : ApiController
+    {
+        SalonDBEntities db = new SalonDBEntities();
+
+        [HttpGet]
+        [Route("api/GetSalonProduct")]
+        public IEnumerable<Admin_Select_SolonProduct_Result> GetSalonProduct()
+        {
+            return db.Admin_Select_SolonProduct().ToList();
+        }
+
+        [HttpPost]
+        [Route("api/AddSalonProduct")]
+        public void AddSalonProduct(SalonProduct product)
+        {
+            try
+            {
+                if (product.ProductName != null)
+                {
+                    db.Admin_Insert_SlonProduct(product.ProductName);
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpPost]
+        [Route("api/UpdateSalonProduct")]
+        public void UpdateSalonProduct(SalonProduct product)
+        {
+            try
+            {
+                if (product.Id > 0)
+                {
+                    db.Admin_Update_SlonProduct(product.Id, product.ProductName);
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpPost]
+        [Route("api/DeleteSalonProduct")]
+        public void DeleteSalonProduct(SalonProduct product)
+        {
+            try
+            {
+                if (product.Id > 0)
+                {
+                    db.Admin_Delete_SolonProduct(product.Id);
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
